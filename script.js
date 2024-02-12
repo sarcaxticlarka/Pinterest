29cd17de502de3b770fe4241d6939e22c4a42871
sarcaxticlarka <140724625+sarcaxticlarka@users.noreply.github.com>
2024-02-12 12:04:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+var arr = [
+    {name: "90's Gate", image: "https://images.unsplash.com/photo-1707501813364-7dbc1c4e97ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "China Rose", image: "https://images.unsplash.com/photo-1519064438923-de4de326dfd1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"},
+    {name: "Aesteics Design", image: "https://images.unsplash.com/photo-1558585918-601f4cf404b0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "Modern Building", image: "https://images.unsplash.com/photo-1707248090756-9bbc90a7a196?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "Building Tower", image: "https://images.unsplash.com/photo-1707155095165-ddc10745ac1e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "Fire Balloon", image: "https://images.unsplash.com/photo-1701616857654-8c56b923d80c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"},
+    {name: "Balloons", image: "https://images.unsplash.com/photo-1693986109563-d8a95d553486?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8"},
+    {name: "White Flower", image: "https://images.unsplash.com/photo-1531131141161-ecdfb1858dd2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zmxvd2Vyc3xlbnwwfHwwfHx8MA%3D%3D"},
+    {name: "Flora Flower", image: "https://images.unsplash.com/photo-1469259943454-aa100abba749?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Pink Rose", image: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D "},
+    {name: "Rose in Glass", image: "https://images.unsplash.com/photo-1575178114667-c8a832c61f45?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D "},
+    {name: "Sports Car", image: "https://images.unsplash.com/photo-1583121274602-3e2820c69888?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D "},
+    {name: "White Car", image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?q=80&w=1883&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "},
+    {name: "Vintage Car", image: " https://images.unsplash.com/photo-1528500474434-2675c175491e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D"},
+    {name: "Rose with Petals", image: "https://images.unsplash.com/photo-1556807182-1a42c64b8283?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D "},
+    {name: "Yellow Car", image: "https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhcnN8ZW58MHx8MHx8fDA%3D "},
+    {name: "Vintage Car", image: " https://images.unsplash.com/photo-1526726538690-5cbf956ae2fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhcnN8ZW58MHx8MHx8fDA%3D"},
+    {name: "Aestetcs Rose", image: "https://images.unsplash.com/photo-1518895949257-7621c3c786d7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cm9zZXxlbnwwfHwwfHx8MA%3D%3D"},
+    {name: "  White Flower", image: "https://images.unsplash.com/photo-1567509773591-8ad6cd6e1031?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D "},
+    {name: "White Rose", image: "https://images.unsplash.com/photo-1652164055648-102e8719ae05?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D "},
+    {name: "Bouquet", image: "https://images.unsplash.com/photo-1591886960528-5dcaeec039b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"},
+    {name: "Rose Bouquet", image: "https://images.unsplash.com/photo-1503652601-557d07733ddc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"},
+    
+]
+
+function showTheCards(){
+    var clutter = "";
+    arr.forEach(function(obj){
+        clutter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="image">
+        <div class="caption">Lorem ipsum </div>
+    </div>`;
+    })
+
+    document.querySelector(".container")
+    .innerHTML = clutter;
+}
+
+function handleSearchFunctionality(){
+    var input = document.querySelector("#searchinput");
+    
+
+    input
+    .addEventListener("focus", function(){
+        document.querySelector(".overlay").style.display = "block";
+    })
+
+    input
+    .addEventListener("blur", function(){
+        document.querySelector(".overlay").style.display = "none";
+    })
+
+    input
+    .addEventListener("input", function(){
+        const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        var clutter = "";
+        filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`
+        })
+    document.querySelector(".searchdata").style.display = "block";
+        document.querySelector(".searchdata").innerHTML = clutter;
+    })
+}
+
+handleSearchFunctionality();
+showTheCards();
